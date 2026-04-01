6a45ff03c2f1175b8734f8d0ba1ce13c609bce71
ablas123 <67266977+ablas123@users.noreply.github.com>
2026-04-02 00:13:41 +0200

    Change database connection to AWS TiDB Cloud
    Updated database connection settings to use new AWS TiDB Cloud credentials and added SSL configuration.

--- sites/default/sqlconf.php
+++ sites/default/sqlconf.php
@@ -3,11 +3,11 @@
 //  OpenEMR
 //  MySQL Config
 
-$host   = 'localhost';
-$port   = '3306';
-$login  = 'openemr';
-$pass   = 'openemr';
-$dbase  = 'openemr';
+$host   = 'gateway01.eu-central-1.prod.aws.tidbcloud.com';
+$port   = '4000';
+$login  = '4Q3JgFwtV72XTzm.root';
+$pass   = 'hCCzJBE3Ko33HuTW';   // استخدم كلمة المرور الصحيحة (هذه هي الافتراضية)
+$dbase  = 'openemr_db';
 
 $sqlconf = [];
 global $sqlconf;
@@ -16,6 +16,11 @@ $sqlconf["port"] = $port;
 $sqlconf["login"] = $login;
 $sqlconf["pass"] = $pass;
 $sqlconf["dbase"] = $dbase;
+
+// إعدادات SSL
+$sqlconf["db_ssl"] = 1;
+$sqlconf["db_ssl_ca"] = '/etc/ssl/certs/ca-tidb.pem';
+$sqlconf["db_ssl_verify"] = 1;
 
 //////////////////////////
 //////////////////////////
