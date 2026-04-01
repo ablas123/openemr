e8e5dc8d1b51e4189fff765d9c6288c7db48c313
ablas123 <67266977+ablas123@users.noreply.github.com>
2026-04-02 00:33:02 +0200

    Update sqlconf.php

--- sites/default/sqlconf.php
+++ sites/default/sqlconf.php
@@ -6,7 +6,7 @@
 $host   = 'gateway01.eu-central-1.prod.aws.tidbcloud.com';
 $port   = '4000';
 $login  = '4Q3JgFwtV72XTzm.root';
-$pass   = 'hCCzJBE3Ko33HuTW';   // your actual password
+$pass   = 'hCCzJBE3Ko33HuTW';   // ضع كلمة المرور الصحيحة إن كنت غيرتها
 $dbase  = 'openemr_db';
 
 $sqlconf = [];
@@ -17,13 +17,13 @@ $sqlconf["login"] = $login;
 $sqlconf["pass"] = $pass;
 $sqlconf["dbase"] = $dbase;
 
-// SSL settings
+// إعدادات SSL
 $sqlconf["db_ssl"] = 1;
 $sqlconf["db_ssl_ca"] = '/etc/ssl/certs/ca-tidb.pem';
 $sqlconf["db_ssl_verify"] = 1;
 
 //////////////////////////
-// Mark installation as complete
-$config = 1; // was 0 before
+// تم الانتهاء من التثبيت
+$config = 1; // كانت 0
 //////////////////////////
 ?>
